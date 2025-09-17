603094ab149a3ca5ac33aaec5fa816ab5110f10b
juan <juanosorio1124226@correo.itm.edu.co>
2025-09-16 19:51:38 -0500

    Agregar archivos de proyecto.

---
+++ ut_presentacion/Nucleo/EntidadesNucleo.cs
@@ -0,0 +1,16 @@
+﻿using lib_dominio.Entidades;
+
+namespace ut_presentacion.Nucleo
+{
+    public class EntidadesNucleo
+    {
+        public static Componentes? Componentes()
+        {
+            var entidad = new Componentes();
+            entidad.Nombre = "Pruebas-" + DateTime.Now.ToString("yyyyMMddhhmmss");
+            entidad.Descripcion = "dhfhfhh";
+
+            return entidad;
+        }
+    }
+}
